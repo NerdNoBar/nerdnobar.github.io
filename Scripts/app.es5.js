f3edd2822e62f4eaa94a4c1ad0d73facfe06125d
leandro <leandro.simoes@outlook.com>
2016-12-04 21:03:10 -0200

    Layout inicial;

---
+++ Scripts/app.es5.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+;(function () {
+    $(document).ready(function () {
+        var post = function post(title, description, audioLink, date) {
+            this.title = title;
+            this.description = description;
+            this.audioLink = audioLink;
+            this.date = date;
+        };
+
+        var $area = $('body'),
+            model = {
+            episodes: ko.observableArray([new post('Second episode!', 'This is the second episode! WOW!', 'https://drive.google.com/file/d/0BxzPq5uDfbejRFlkWFZHM1B4dnc/preview', '04/12/2016'), new post('Pilot!', 'This is the first episode! WOW!', 'https://drive.google.com/file/d/0BxzPq5uDfbejX0Y4eW5qdDF4Z3c/preview', '03/12/2016')]),
+            selectedEpisode: ko.observable()
+        };
+
+        ko.applyBindings(model, $area[0]);
+    });
+})();
+
